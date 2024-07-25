7a1ae14b08ffdc36bcbd20c4402b3bb63769130c
Gimling <huangjl@ruyi.ai>
2024-07-25 19:03:53 +0800

    debug

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ source "/root/.sdkman/bin/sdkman-init.sh"
 sdk install java 11.0.24-zulu
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-. /root/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install 20
 
