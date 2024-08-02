00d142925e65bcd974dfadd269e77ce28e945168
Gimling <huangjl@ruyi.ai>
2024-08-02 09:54:12 +0800

    update 18-py39

--- huawei-py39/install_sys_lib_and_conda.sh
+++ huawei-py39/install_sys_lib_and_conda.sh
@@ -16,3 +16,15 @@ bash miniconda.sh -b -p /root/miniconda3
 rm -f miniconda.sh
 
 /root/miniconda3/bin/conda init bash
+
+
+apt install -y software-properties-common
+add-apt-repository ppa:ubuntu-toolchain-r/test
+apt install -y gcc-9
+apt install -y g++-9
+apt install git
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9
+
+
+curl -O -L https://github.com/Kitware/CMake/releases/download/v3.30.1/cmake-3.30.1-linux-aarch64.tar.gz
+tar xzf cmake-3.30.1-linux-aarch64.tar.gz
