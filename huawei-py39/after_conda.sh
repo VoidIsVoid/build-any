19e7f1e1b8e0445dcf19c8213317acd027e5fc42
Gimling <huangjl@ruyi.ai>
2024-08-02 08:28:33 +0800

    update bash script

--- huawei-py39/after_conda.sh
+++ huawei-py39/after_conda.sh
@@ -1,9 +1,9 @@
 set -e
 source /root/miniconda3/bin/activate base
-conda create -n py39 'python=3.9.2'
+conda create -n py39 'python=3.9' -y
 echo 'source /root/miniconda3/bin/activate py39' >> "/root/.bashrc"
 source /root/miniconda3/bin/activate py39
-conda install -y gcc_linux-64=9 gxx_linux-64=9 cmake=3.26 git
+conda install -y gcc_linux-aarch64=11 gxx_linux-aarch64=11 cmake=3.26 git
 
 ln -s $GCC /usr/local/bin/gcc
 ln -s $GXX /usr/local/bin/g++
--- huawei-py39/install_sys_lib_and_conda.sh
+++ huawei-py39/install_sys_lib_and_conda.sh
@@ -1,17 +1,18 @@
 set -e 
 
-arch=$(uname -m) && \
-    if [ "$arch" = "x86_64" ]; then \
-    MINICONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"; \
-    elif [ "$arch" = "aarch64" ]; then \
+arch=$(uname -m) 
+echo "arch $arch"
+if [ "$arch" = "x86_64" ]; then 
+    MINICONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"; 
+elif [ "$arch" = "aarch64" ]; then 
     MINICONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh"; \
-    else \
-    echo "Unsupported architecture: $arch"; \
-    exit 1; \
-    fi && \
-    wget $MINICONDA_URL -O miniconda.sh --quiet && \
-    mkdir -p /root/conda && \
-    bash miniconda.sh -b -p /root/miniconda3 && \
-    rm -f miniconda.sh
- 
+else 
+    echo "Unsupported architecture: $arch"; 
+    exit 1; 
+fi 
+wget $MINICONDA_URL -O miniconda.sh --quiet
+mkdir -p /root/conda
+bash miniconda.sh -b -p /root/miniconda3
+rm -f miniconda.sh
+
 /root/miniconda3/bin/conda init bash
