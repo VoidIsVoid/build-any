641d09d6599d6dd28fbca6a4fd28c09b7cd66fd4
Gimling <huangjl@ruyi.ai>
2024-09-07 10:46:57 +0800

    build cuda

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+set -e
+cd $(cat current)
+sudo sed -i -z 's/\(.*\)}\(.*\)/\1,"data-root":"\/mnt\/docker" }\2/' /etc/docker/daemon.json
+sudo systemctl restart docker
+
+FILE="run.sh"
+
+if [ -f "$FILE" ]; then
+    exec "$FILE"
+else
+    docker buildx build --push -t ${{ secrets.DOCKERHUB_USERNAME }}/$(cat docker-name.txt):$(cat docker-tag.txt) .
+fi
