bf149cea6ffc598c086c1b44da6e03ef61ae200e
Gimling <huangjl@ruyi.ai>
2024-09-27 22:50:37 +0800

    add llama.cpp

---
+++ llamacpp/run.sh
@@ -0,0 +1,6 @@
+set -e 
+git clone https://github.com/ggerganov/llama.cpp.git
+cd llama.cpp
+tag=$(git describe --tags $(git rev-list --tags --max-count=1))
+cd ..
+docker buildx build --push -t ${DOCKER_USERNAME}/$(cat docker-name.txt):$tag .
